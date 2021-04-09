b1d77cad65eaeb86c50304f650187a45b0c4a595
MetraDZ <38877639+MetraDZ@users.noreply.github.com>
2021-04-09 17:35:59 +0200

    Update models.py

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from .validators import validate_email, validate_message
     
 class Message(models.Model):
-    content =   models.CharField(max_length = 100, null = True, validators = [validate_message])
-    author =    models.CharField(max_length = 30, null = True, validators = [validate_email])
+    content =   models.CharField(max_length = 100, default = 'Nothing to see...', validators = [validate_message])
+    author =    models.CharField(max_length = 30, default = 'Unknown user', validators = [validate_email])
     send_time = models.DateTimeField(auto_now_add = True, null = True)
     
     def __str__(self):
