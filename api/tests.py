65a0d85d4e867bd960642de5e61e9c92e5629104
MetraDZ <38877639+MetraDZ@users.noreply.github.com>
2021-04-09 18:21:14 +0200

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Message
 
-# Create your tests here.
+class MessageTestCase(TestCase):
+    def setUp(self):
+        self.message = Message.objects.create(author = 'author@gmail.com,', content = 'Author')
+    
+    def test_message_is_sent(self):
+        message = Message.objects.get(content = 'Author')
+        self.assertEqual(message, self.message)
+    
+    def tearDown(self):
+        self.message.delete()
